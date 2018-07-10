50be10c3e0de6e84553b4ef22c79fd017ce6ee87
Jesus <alv.mtz94@gmail.com>
2018-07-10 14:24:21 +0200

    added sessions functionality
    Co-authored-by: Ayoub Abed <skobraf@gmail.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,24 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:sessions][:email].downcase)
+
+    if user && user.authenticate(params[:session][:password])
+      #login and redirect
+      log_in(user)
+      
+    else
+      #show message
+      flash.now[:danger] = 'Invalid Email/Password Combination'
+      render 'new'
+    end 
+
+  end
+
+  private
+
+  def whitelist_params
+  end
 end
