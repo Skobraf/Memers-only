13f3cc6e1f41afd45ea9863896c6151fc0536bfa
Jesus <alv.mtz94@gmail.com>
2018-07-16 17:51:08 +0200

    added remember me checkbox and methods to user model
    Co-authored-by: Ayoub Abed <skobraf@gmail.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:sessions][:password])
       #login and redirect
       log_in(user)
-      remember user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
     else
       #show message
@@ -16,6 +16,11 @@ class SessionsController < ApplicationController
       render 'new'
     end 
 
+  end
+
+  def destroy
+    log_out if logged_in?
+    redirect_to root_url
   end
 
   private
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -33,17 +33,15 @@ module SessionsHelper
 
     # Logs out the current user.
     def log_out
+        forget(current_user)
         session.delete(:user_id)
         @current_user = nil
     end
 
-    # Returns true if the given token matches the digest.
-    def authenticated?(remember_token)
-        BCrypt::Password.new(remember_digest).is_password?(remember_token)
-    end
-
-    # Forgets a user.
-    def forget
-        update_attribute(:remember_digest, nil)
+    # Forgets a persistent session.
+    def forget(user)
+        user.forget
+        cookies.delete(:user_id)
+        cookies.delete(:remember_token)
     end
 end
